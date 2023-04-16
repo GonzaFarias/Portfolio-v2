416af9aea4e61e297ffa79142c226bf685594464
Gonzalo Farías <gonzaxeneize01@gmail.com>
2023-04-15 23:10:01 -0300

    Add files via upload

--- js/function2.js
+++ js/function2.js
@@ -48,3 +48,60 @@ function showContent() {
 
     }
 }
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    var fadeIns = document.querySelectorAll(".fade-in");
+    
+    function fadeInHandler() {
+      for (var i = 0; i < fadeIns.length; i++) {
+        var element = fadeIns[i];
+        if (isElementInViewport(element)) {
+          element.classList.add("active");
+        }
+      }
+    }
+    
+    function isElementInViewport(el) {
+      var rect = el.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+    }
+    
+    window.addEventListener("scroll", fadeInHandler);
+    window.addEventListener("load", fadeInHandler);
+  });
+
+  const slideIn = document.querySelector('.slide-in');
+
+  function debounce(func, wait = 20, immediate = true) {
+    let timeout;
+    return function () {
+      const context = this, args = arguments;
+      const later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  }
+  
+  function checkSlide() {
+    const slideInAt = (window.scrollY + window.innerHeight) - slideIn.offsetHeight / 2;
+    const isHalfShown = slideInAt > slideIn.offsetTop;
+    if (isHalfShown) {
+      slideIn.classList.add('active');
+    } else {
+      slideIn.classList.remove('active');
+    }
+  }
+  
+  window.addEventListener('load', debounce(checkSlide));
+  
+  
